9f2a24240d8bea9b9ced14ee4e7f2a9080018f0e
Justin Sorensen <sorensenjg@gmail.com>
2018-04-27 00:30:46 -0700

    Testing feature/login branch subdomain

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -48,7 +48,7 @@ class Home extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                            <h1>Github Battle</h1>
+                            <h1>Github Battle - Feature/Login</h1>
                             <p className="lead">Test your skills, battling it out with your fellow githubbers. Prepare for death or glory!</p>
                             <Link to="/playerOne" className="btn btn--primary type--uppercase">
                                 <span className="btn__text">Get started</span>
